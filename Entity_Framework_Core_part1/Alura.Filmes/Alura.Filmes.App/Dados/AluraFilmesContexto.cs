77d04d2a96579ae694388e8654c3fa8fca0898d3
Bruno Costa e Silva <brunocosta.1991@hotmail.com>
2021-04-11 16:49:47 -0300

    Subindo repo

---
+++ Entity_Framework_Core_part1/Alura.Filmes/Alura.Filmes.App/Dados/AluraFilmesContexto.cs
@@ -0,0 +1,29 @@
+﻿using Alura.Filmes.App.Negocio;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Alura.Filmes.App.Dados
+{
+    class AluraFilmesContexto : DbContext
+    {
+        public DbSet<Ator> Atores { get; set; }
+        public DbSet<Filme> Filmes { get; set; }
+        public DbSet<FilmeAtor> Elenco { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Idioma> Idiomas { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=AluraFilmes;Trusted_connection=true;");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new AtorConfiguration());
+            modelBuilder.ApplyConfiguration(new FilmeConfiguration());
+            modelBuilder.ApplyConfiguration(new FilmeAtorConfiguration());
+            modelBuilder.ApplyConfiguration(new CategoriaConfiguration());
+            modelBuilder.ApplyConfiguration(new FilmeCategoriaConfiguration());
+            modelBuilder.ApplyConfiguration(new IdiomaConfiguration());
+        }
+    }
+}
---
+++ Entity_Framework_Core_part1/Alura.Filmes/Alura.Filmes.App/Negocio/FilmeCategoria.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alura.Filmes.App.Negocio
+{
+    public class FilmeCategoria
+    {
+        public Categoria Categoria { get; set; }
+        public Filme Filme { get; set; }
+    }
+}
